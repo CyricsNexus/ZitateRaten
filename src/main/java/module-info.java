1fab0c7085931fd063619faa50411cf72c644c60
Carmen <10047401o@gmx.de>
2026-03-11 21:22:23 +0100

    Version 1.0, Doku folgt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ZitateRaten.main {
+/*****
+ *  Ein Spiel, um Zitate zu erraten
+ *  Zu Beginn wird ein Zitat angezeigt und 5 Sekunden später der Urheber, bis alle Zitate
+ *  durchgegangen sind.
+ */
+}
